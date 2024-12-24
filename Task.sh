cb2bb7d072ba738a496018504920bc95adcb8691
noveberme <evgeniyagagra@gmail.com>
2024-12-24 21:53:15 +0300

    Add files via upload

--- Task.sh
+++ Task.sh
@@ -1,15 +1,13 @@
-#7.Сохранить в файл выдержку из мануала к команде ls (man ls), в которой будет описано использование ключа -r.
+# 7. Сохранить в файл выдержку из мануала к команде ls (man ls), в которой будет описано использование ключа -r.
 #!/bin/bash
 
 output_file="output.txt"
 
-result=$(man ls | awk '/-r, --reverse/{flag=1} flag && /^[[:space:]]*$/ {flag=0} flag')
+man ls | grep -A 5 '\-r' > "$output_file"
 
-echo "$result" > "$output_file"
-
-if [[ -s $output_file ]]; then
-  echo "Выдержка успешно сохранена в файл $output_file."
-else
-  echo "Ошибка: не удалось извлечь выдержку или она пуста."
+if [ -s $output_file ]; then 
+ echo "Выдержка успешно сохранена в файл $output_file." 
+else 
+ echo "Ошибка: не удалось извлечь выдержку или она пуста." 
 fi
 
